81b1666daab6bd23915523e2f989ab240bb13b20
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-01 03:05:18 -0300

    Creamos we

---
+++ src/components/We.jsx
@@ -0,0 +1,11 @@
+const We = () =>{
+   return (
+      <div>
+         <h2>We</h2>
+         <p>We are a company dedicated to Technologies, where we try to help all people. This website is a test and we accept your criticism and opinions.</p>
+         <p>We have been in the industry since 2020 offering our products</p>
+      </div>
+   )
+};
+
+export {We};
