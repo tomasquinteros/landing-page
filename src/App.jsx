15678d0926260c2841ebc66c57c952c310214c63
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-01 18:33:18 -0300

    Cambiamos ruta path de Contacto

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Header } from './components/Header';
 import { Product } from './components/products/Product';
 import { Welcome } from './components/Welcome';
 import { We } from './components/We';
-import { Contact } from './components/Contact';
+import { Contact } from './components/Contact/Contact';
 function App() {
 
   return (
