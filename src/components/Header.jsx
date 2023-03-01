135bcaebe89ed64c7e1274ac9db0e402f35c0e2a
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-01 03:04:29 -0300

    Estilizamos header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,33 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
-
-
 const Header = () => {
-   const active = "bg-sky-300"
-   const navBarStay = "bg-transparent"
+   const styleNav = "py-8 flex justify-around fixed items-center transition duration-1000 all -z-40 top-0 left-0 min-w-full";
+   const active = styleNav + " bg-black";
+   const navBarStay = styleNav + " bg-transparent"; 
    const [navBar, setNavBar] = useState(false);
    const changeBg = () => {
-      if (window.scrollY > 100) {
+      if (window.scrollY > 180) {
          setNavBar(true);
+      }else {
+         setNavBar(false)
       }
-      window.addEventListener("scroll", changeBg);
    }
-   const changeBackground = navBar ? active : navBarStay;
+   window.addEventListener("scroll", changeBg);
 
-   const styleHeader = "py-4 flex justify-around items-center min-w-max"
    return (
-      <nav 
-      className={`${styleHeader} ${changeBackground}`}
-      >
+      <div className="mb-32">
+         <nav 
+            className={navBar ? active : navBarStay}
+         >
          <NavLink to="/" className="text-3xl">Tecnologies Store</NavLink>
          <div className="flex gap-10 ">
             <NavLink to="/" className="text-xl text-cyan-300 hover:text-neutral-200">We</NavLink>
             <NavLink to="/" className="text-xl text-cyan-300 hover:text-neutral-200">Product</NavLink>
             <NavLink to="/" className="text-xl text-cyan-300 hover:text-neutral-200">Contact</NavLink>
-         </div>
-      </nav>
+            </div>
+         </nav>
+      </div>
    )
 
 }
