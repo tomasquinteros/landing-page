e9fc4252ccb74e12461efacd1e0ed81034309587
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-02 02:17:09 -0300

    pusimos id

--- src/components/We.jsx
+++ src/components/We.jsx
@@ -1,9 +1,19 @@
 const We = () =>{
    return (
-      <div>
-         <h2>We</h2>
+      <div id="We" className="flex flex-col gap-20">
+         <h2 className="text-6xl">We</h2>
+         <div className="text-xl font-light w-3/4">
          <p>We are a company dedicated to Technologies, where we try to help all people. This website is a test and we accept your criticism and opinions.</p>
          <p>We have been in the industry since 2020 offering our products</p>
+         </div>
+         <div>
+
+         <h2 className="text-2xl">What do we aspire to?</h2>
+         <p className="text-lg font-extralight">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit accusamus in dolor. Mollitia quidem distinctio ex autem quisquam provident illo numquam molestias? Minima, sapiente porro. Adipisci dolores debitis nihil voluptates.
+         Rerum debitis provident fugit optio accusantium laboriosam nemo odio ducimus dolor cupiditate at explicabo, labore praesentium expedita necessitatibus quibusdam quis perferendis exercitationem. Expedita ex corrupti deleniti? Tempore blanditiis cupiditate sapiente?
+         Aperiam nemo odio modi fugiat! Mollitia laboriosam dolorem quam reprehenderit repellendus molestias quod, et beatae enim possimus saepe. Optio earum expedita voluptatem, repellat dignissimos provident deleniti illo. Natus, cumque laudantium.</p>
+         <img src="" alt="" />
+         </div>
       </div>
    )
 };
