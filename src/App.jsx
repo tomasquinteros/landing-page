8720e5ca21a66c415876f823d34d35bf83cde505
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-02 02:15:46 -0300

    change links y move todos los comp to browserroute

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter , Route } from 'react-router-dom';
+import { BrowserRouter, Route , Routes } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Product } from './components/products/Product';
 import { Welcome } from './components/Welcome';
@@ -10,13 +10,14 @@ function App() {
     <div className="flex flex-col justify-center gap-20 mx-auto">
       <BrowserRouter>
           <Header/>
+          <main className='w-3/4 mx-auto flex flex-col gap-36'>
+            <Welcome/>
+            <Product/>
+            <We/>
+            <Contact/>
+          </main>
       </BrowserRouter>
-      <main className='w-3/4 mx-auto'>
-        <Welcome/>
-        <Product/>
-        <We/>
-        <Contact/>
-      </main>
+      
     </div>
   )
 }
