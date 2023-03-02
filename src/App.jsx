0dd5b4a43c45b14519cd91d2df80a5337941d559
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-02 03:45:36 -0300

    create a button t0 go to

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { BrowserRouter, Route , Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Product } from './components/products/Product';
 import { Welcome } from './components/Welcome';
 import { We } from './components/We';
 import { Contact } from './components/Contact/Contact';
+import {BiUpArrow} from 'react-icons/bi'
 function App() {
 
   return (
-    <div className="flex flex-col justify-center gap-20 mx-auto">
+    <div id="top" className="flex flex-col justify-center gap-20 mx-auto">
       <BrowserRouter>
           <Header/>
           <main className='w-3/4 mx-auto flex flex-col gap-36'>
@@ -17,7 +18,9 @@ function App() {
             <Contact/>
           </main>
       </BrowserRouter>
-      
+      <a href='#top' className='z-30 fixed right-4 bottom-4 bg-gray-500 p-2'>
+        <BiUpArrow className='text-cyan-400 text-3xl'/>
+      </a>
     </div>
   )
 }
