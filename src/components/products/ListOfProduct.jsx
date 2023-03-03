cb385c710019b35c331185bdaeca4aa1d7a160ae
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-03 03:08:49 -0300

    destructuring json info

--- src/components/products/ListOfProduct.jsx
+++ src/components/products/ListOfProduct.jsx
@@ -1,20 +1,22 @@
 import { HashLink as Link } from "react-router-hash-link";
 
-const ListOfProduct = ({products}) => {
+const ListOfProduct = ({ products }) => {
    return (
-      <div className="grid grid-cols-3 gap-12 flex-wrap">
-         {products.map(product => (
-            <div key={product.id} className="flex flex-col justify-center items-center gap-4 border border-slate-900 rounded-md p-4 hover:scale-110 transition bg-slate-800">
-               <img src={product.img} alt={product.id+product.name} className="w-3/4"/>
-               <h3 className="text-xl">{product.name}</h3>
-               <p>{product.description}</p>
-               <div className="flex flex-col gap-4 text-center">
-                  <a className="bg-gray-900 p-3 rounded-md" href={product.link}>More info</a>
-                  <Link className="border p-3 rounded-md" to="#Contact">Contact</Link>
+      <div className="flex flex-col lg:grid xl:grid-cols-3 lg:grid-cols-2 gap-12">
+         {products.map(({ id, image, info, link, name }) => (
+            <div key={id} className="flex flex-col gap-4 shadow-2xl items-center justify-center rounded-md p-4 hover:scale-105 transition bg-slate-800">
+               <div className="h-3/4">
+                  <img src={image} alt={id + name} className="w-3/4 max-h-64 m-auto" />
+               </div>
+               <h3 className="text-xl">{name}</h3>
+               <p className="font-light lg:text-center">{info}</p>
+               <div className="flex gap-4 items-center justify-between flex-col md:flex-row">
+                  <a className="bg-gray-900 p-3 rounded-md hover:bg-transparent hover:border" href={link} target="_blank">More info</a>
+                  <Link className="border p-3 rounded-md hover:border-cyan-300 hover:text-cyan-300" to="#Contact">Contact</Link>
                </div>
             </div>
          ))}
       </div>
    )
-} 
-export {ListOfProduct};
+}
+export { ListOfProduct };
