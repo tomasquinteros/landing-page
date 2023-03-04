296821bde68424ae22a9f7137aa81a53105c7987
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-04 14:35:25 -0300

    import error component

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@
 import { BrowserRouter } from 'react-router-dom';
 import { BiUpArrow } from 'react-icons/bi'
 import { waveBottom, waveTop } from './data/styles';
-
 /*Import Components */
 import { Header } from './components/Header';
 import { Product } from './components/products/Product';
 import { Welcome } from './components/Welcome';
 import { We } from './components/We';
 import { Contact } from './components/Contact/Contact';
-
+import { Error404 } from './components/Error404';
 
 /* App */
 function App() {
+
   return (
     <div id="top" className="flex flex-col justify-center mx-auto">
+      <Error404/>
       <BrowserRouter>
         <Header />
         <main className='mx-auto flex flex-col items-center md:gap-20'>
@@ -39,7 +40,7 @@ function App() {
         <BiUpArrow className='text-cyan-400 text-3xl' />
       </a>
     </div>
-  )
-}
+    )
+  }
 
 export default App
