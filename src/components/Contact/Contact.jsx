af016cb63837ae67422d8b05a9006f8a8cf40dea
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-05 13:07:30 -0300

    Import wave and styles o wave

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,38 +3,43 @@ import { AiOutlineCloseCircle } from "react-icons/ai"
 import { Form } from "./Form";
 import { motion } from "framer-motion"
 import {FastContact} from "./FastContact";
-
+import { waveTop } from "../../data/styles";
 
 const Contact = () => {
    const [openModal, setOpenModal] = useState(false);
    return (
-      <div id="Contact" className="flex flex-col gap-5 w-3/4 mb-12">
-         <h2 className="text-4xl md:text-6xl font-medium text-white border-b p-8">Contact</h2>
-         <div className="flex flex-col md:flex-row items-center justify-between gap-20 p-8">
-            <FastContact/>
-            <div className="flex flex-col gap-4">
-               <button 
-                  className="btn bg-slate-800 p-6 rounded-md hover:scale-125 transition-all shadow-2xl"
-                  onClick={() => { setOpenModal(!openModal) }}
-               >
-                  Send Messege
-               </button>
+      <div className="w-full">
+         {waveTop}
+         <div className='flex flex-col items-center bg-blue-700'>
+            <div id="Contact" className="flex flex-col gap-5 w-3/4 mb-12">
+               <h2 className="text-4xl md:text-6xl font-medium text-white border-b p-8">Contact</h2>
+               <div className="flex flex-col md:flex-row items-center justify-between gap-20 p-8">
+                  <FastContact/>
+                  <div className="flex flex-col gap-4">
+                     <button 
+                        className="btn bg-slate-800 p-6 rounded-md hover:scale-125 transition-all shadow-2xl"
+                        onClick={() => { setOpenModal(!openModal) }}
+                        >
+                        Send Messege
+                     </button>
+                  </div>
+               </div>
+               {openModal ?
+                  <div className="z-40 bg-black/95 fixed top-0 left-0 w-screen h-screen flex justify-center items-center">
+                     <motion.div 
+                        className="flex flex-col items-center justify-between bg-blue-700 p-2 rounded-sm shadow-2xl w-3/4 lg:w-auto md:full"
+                        initial={{ scale: 0 }}
+                        animate={{ rotate: 0, scale: 1 }}
+                        transition={{ type: "spring" }}>
+                        <button className="ml-auto text-cyan-400" onClick={() => { setOpenModal(!openModal) }}>
+                           <AiOutlineCloseCircle className="text-3xl md:text-5xl relative top-0" />
+                        </button>
+                        <Form />
+                     </motion.div>
+                  </div>
+                  : null}
             </div>
          </div>
-         {openModal ?
-            <div className="z-40 bg-black/95 fixed top-0 left-0 w-screen h-screen flex justify-center items-center">
-               <motion.div 
-                  className="flex flex-col items-center justify-between bg-blue-700 p-2 rounded-sm shadow-2xl w-3/4 lg:w-auto md:full"
-                  initial={{ scale: 0 }}
-                  animate={{ rotate: 0, scale: 1 }}
-                  transition={{ type: "spring" }}>
-                  <button className="ml-auto text-cyan-400" onClick={() => { setOpenModal(!openModal) }}>
-                     <AiOutlineCloseCircle className="text-3xl md:text-5xl relative top-0" />
-                  </button>
-                  <Form />
-               </motion.div>
-            </div>
-            : null}
       </div>
    )
 }
--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -1,19 +1,26 @@
 import { ListOfProduct } from "./ListOfProduct";
 import products from "../../data/product.json"
-import { useLocation } from 'react-router-dom';
+import { waveBottom, waveTop } from '../../data/styles';
+
+
 const Product = () => {
-   console.log(useLocation())
 
    return (
-      <section id="product" className="flex flex-col items-center justify-center gap-8 mt-16 min-h-screen w-3/4 mb-12 lg:mb-0">
-         <h2 className="text-6xl">Our Products</h2>
-         <p className="text-xl text-slate-400">Some product we have in stock </p>
-         <div className="lg:p-12">
-            <article>
-               <ListOfProduct products={products} />
-            </article>
+      <div id='wave-container' className="w-full">
+         {waveTop}
+         <div className='flex flex-col items-center bg-blue-700'>
+            <section id="product" className="flex flex-col items-center justify-center gap-8 mt-16 min-h-screen w-3/4 mb-12 lg:mb-0">
+               <h2 className="text-6xl">Our Products</h2>
+               <p className="text-xl text-slate-400">Some product we have in stock </p>
+               <div className="lg:p-12">
+                  <article>
+                     <ListOfProduct products={products} />
+                  </article>
+               </div>
+            </section>
          </div>
-      </section>
+         {waveBottom}
+      </div>
    )
 }
 
