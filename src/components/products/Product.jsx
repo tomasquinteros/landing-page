97d02744c4fa2168514971a3127838fb9a20f217
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-06 17:40:37 -0300

    change styles

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -9,7 +9,7 @@ const Product = () => {
       <div id='wave-container' className="w-full">
          {waveTop}
          <div className='flex flex-col items-center bg-blue-700'>
-            <section id="product" className="flex flex-col items-center justify-center gap-8 mt-16 min-h-screen w-3/4 mb-12 lg:mb-0">
+            <section id="Product" className="flex flex-col items-center justify-center gap-8 mt-16 min-h-screen w-3/4 mb-12 lg:mb-0">
                <h2 className="text-6xl">Our Products</h2>
                <p className="text-xl text-slate-400">Some product we have in stock </p>
                <div className="lg:p-12">
