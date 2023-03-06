58d9541456abf3b5f0b72510f24fc416e061dbce
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-06 18:11:32 -0300

    error change path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 /* Import Dependencies */
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Routen, NotFoundRoute } from 'react-router-dom';
 import { LandingPage } from './components/LandingPage';
 import { Error404 } from './components/Error404';
 import { Header } from './components/Header';
@@ -14,7 +14,7 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<LandingPage />} />
-        <Route path='/*' element={<Error404 />} />
+        <Route exact path='*' element={<Error404 />} />
       </Routes>
     </BrowserRouter>
   )
