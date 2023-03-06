d8ed78045fac75e26402b8976a0dc003a385e776
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-06 17:41:46 -0300

    create LandingPage component

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -8,22 +8,22 @@ import goToTop from './goToTop'
 
 const LandingPage = () => {
 
-   return (
-      <div className="flex flex-col justify-center mx-auto">
-        <main className='mx-auto flex flex-col items-center md:gap-20'>
-          <Welcome />
-          <Product/>
-          <We />
-        </main>
-        <footer className='w-full'>
-            <Contact />
-        </footer>
+  return (
+    <div className="flex flex-col justify-center mx-auto">
+      <main className='mx-auto flex flex-col items-center md:gap-20'>
+        <Welcome />
+        <Product />
+        <We />
+      </main>
+      <footer className='w-full'>
+        <Contact />
+      </footer>
       <a href='#top' onClick={goToTop()} className='z-30 fixed right-4  bottom-4 bg-green-400/50 p-2'>
         <GrLinkTop className=' text-2xl' />
       </a>
-      </div>
-   )
+    </div>
+  )
 
 }
 
-export {LandingPage}
+export { LandingPage }
