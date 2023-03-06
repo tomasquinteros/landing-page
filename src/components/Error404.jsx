114e3f25c1080444397a374bd94acafbe9548572
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-06 17:40:54 -0300

    completed component Error404

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,6 +1,14 @@
+import { Link } from "react-router-dom"
+import {FaHome} from "react-icons/fa"
 
 export const Error404 = () => {
    return (
-      <div><h1>PAGE NOT FOUND</h1></div>
+      <div className="lg:w-3/4 flex flex-col justify-center items-center mx-auto gap-8">
+         <h1 className="text-center text-6xl lg:text-8xl">PAGE NOT FOUND</h1>
+         <Link className="flex items-center gap-2 text-xl" to="/"> 
+            <FaHome/>
+            BACK TO PAGE
+         </Link>
+      </div>
    )
 }
