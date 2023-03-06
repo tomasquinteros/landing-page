b92ea5510990ce4c98a79e78f7b37f7d23b0b918
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-06 17:39:56 -0300

    create buttonContact component

---
+++ src/components/products/ButtonContact.jsx
@@ -0,0 +1,5 @@
+import { HashLink as Link } from "react-router-hash-link";
+const ButtonContact = () => {
+   return <Link className="border p-3 rounded-md bg-gray-900 border-gray-900  hover:border-green-400 hover:text-green-400" to="#Contact">Contact</Link>
+}
+export {ButtonContact}
